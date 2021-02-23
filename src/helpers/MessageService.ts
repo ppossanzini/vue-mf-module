cd94d290fd6fdf4e9f15ef16fe6c50ee8a2ceabb
ppossanzini <paolo@teamdev.it>
2021-02-23 19:27:08 +0100

    ask and reply

--- src/helpers/MessageService.ts
+++ src/helpers/MessageService.ts
@@ -23,4 +23,26 @@ export class MessageService {
   unsubscribe(message: string, callback?: Function) {
     this.notifier.off(message, callback);
   }
+
+  ask<T>(message: string, ...args: any[]): Promise<T> {
+    return new Promise((resolve, reject) => {
+      this.notifier.emit(`$ask-${message}`, {
+        resolve,
+        reject,
+        args
+      })
+    })
+  }
+
+  reply<R>(message: string, callback: (...args: any[]) => R) {
+    this.notifier.on(`$ask-${message}`, (m: { resolve: (data: R) => void, reject: Function, args: any[] }) => {
+      try {
+        let result = callback(...m.args);
+        m.resolve(result);
+      }
+      catch (err) {
+        m.reject();
+      }
+    });
+  }
 }
