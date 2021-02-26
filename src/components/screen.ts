9f021b488682aef42811b5262031b9a1ae3439a6
ppossanzini <paolo@teamdev.it>
2021-02-26 17:18:01 +0100

    fix screen show

--- src/components/screen.ts
+++ src/components/screen.ts
@@ -5,14 +5,13 @@ import { IProjectableModel, Projector } from "../helpers/Projector";
 @Component({
   props: {
     name: { type: String, default: "defaultscreen" },
-    model: { default: null }
   },
   template: `<div v-show="isVisible"><component v-if="currentView" v-bind:is="currentView" :value="model"></component></div>`
 })
 export default class Screen extends Vue {
   name!: string;
   currentView: any = null;
-  model!: IProjectableModel<any> | null;
+  model: IProjectableModel<any> | null = null;
 
   get isVisible() {
     return this.currentView != null || (this.$el && this.$el.children && this.$el.children.length);
