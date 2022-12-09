69df2dbcc2b3b14a3d985bf77fe56fc7482f8430
Paolo Possanzini <paolo@teamdev.it>
2022-12-09 17:27:20 +0100

    fix

--- src/components/screen.ts
+++ src/components/screen.ts
@@ -6,12 +6,16 @@ import { IProjectableModel, Projector } from "../helpers/Projector";
   props: {
     name: { type: String, default: "defaultscreen" },
   },
-  template: `<div v-show="isVisible"><component v-if="currentView" v-bind:is="currentView" :value="model" :key="model"></component></div>`
+  template: `<div v-show="isVisible"><component v-if="currentView" v-bind:is="currentView" :value="model" :key="key"></component></div>`
 })
 export default class Screen extends Vue {
   name!: string;
   currentView: any = null;
   model: IProjectableModel<any> | null = null;
+
+  get key() {
+    return `id-${this.model}`
+  }
 
   get isVisible() {
     return this.currentView != null;
